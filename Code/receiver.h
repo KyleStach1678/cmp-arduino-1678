faa6dd6e76a85c44cbb20b25f0817274929a4b8d
Kylestach1678 <Kylestach99@gmail.com>
2015-05-13 17:39:02 -0700

    Experimental RF receiver - untested.

--- Code/receiver.h
+++ Code/receiver.h
@@ -4,6 +4,36 @@
 #define IR_CODE_PAUSE 0xC285
 #define IR_CODE_STOP 0xC286
 
+#define RF_LOWER_START 40
+#define RF_UPPER_START 80
+
+class RFFieldSystem {
+    int state;
+    int rcvpin;
+ public:
+    RFFieldSystem(int pin) : rcvpin(pin) {
+      state = 0;
+    }
+    void WaitForStart() {
+      delay(10);
+      while (state != 1) {
+        int data = analogRead(rcvpin);
+        if (data < RF_LOWER_START && data > RF_UPPER_START) {
+          state = 1;
+        }
+      }
+    }
+    void Update() {
+        int data = analogRead(rcvpin);
+        if (data > RF_LOWER_START || data < RF_UPPER_START) {
+          state = 0;
+        }
+    }
+    bool IsRunning() {
+      return state == 1;
+    }
+};
+
 class IRFieldSystem {
     int state;
     IRrecv receiver;
@@ -15,6 +45,7 @@ class IRFieldSystem {
     }
     void WaitForStart() {
       while (state != 1) {
+        delay(10);
         if (receiver.decode(&results)) {
           Serial.print("Value from IR: "); Serial.print(results.value, HEX);
           if (results.value == IR_CODE_START) {
@@ -22,7 +53,6 @@ class IRFieldSystem {
           }
           receiver.resume();
         }
-        delay(10);
       }
     }
     void Update() {
